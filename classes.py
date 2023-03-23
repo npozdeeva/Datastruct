b7a0c48e763e7a4ceaab09f3840e60491c0f1033
natalie <natalie@example.com>
2023-03-24 01:11:23 +0300

    task2

--- classes.py
+++ classes.py
@@ -1,18 +1,27 @@
 class Node:
-    def __init__(self, data, link_to_next=None):
+    def __init__(self, data=None, link_to_next=None):
         self.data = data
         self.link_to_next = link_to_next
 
 class Stack:
     def __init__(self):
-        self.to_next = None
+        self.to_next = Node()
 
     def push(self, data):
         new_node = Node(data)
         new_node.link_to_next = self.to_next
         self.to_next = new_node
 
+    def link_to_next(self):
+        return self.link_to_next()
 
-# n1 = Node(5, None)
-# n2 = Node('a', n1)
-# print(n1.data)
+    def to_next(self):
+        return self.to_next()
+
+    def data(self):
+        return self.data()
+
+    def pop(self):
+        remove = self.to_next
+        self.to_next = self.to_next.link_to_next
+        return remove.data
